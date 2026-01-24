b2ac48f303f7e37bb9afd6a18360ec7b2a76575e
ChakornK <chakornk2007@gmail.com>
2026-01-24 00:10:05 -0800

    feat: edit page

---
+++ src/app/edit/page.tsx
@@ -0,0 +1,10 @@
+export default function Editor() {
+  return (
+    <main className="h-dvh flex w-screen items-stretch divide-x overflow-hidden">
+      <div className="w-1/2 p-8">
+        <h1 className="text-3xl font-semibold">Edit your resume</h1>
+      </div>
+      <div className="w-1/2 overflow-auto p-8"></div>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <main className="h-dvh flex w-screen flex-col items-center justify-center gap-2 overflow-hidden bg-black text-white">
+    <main className="h-dvh flex w-screen flex-col items-center justify-center gap-2 overflow-hidden">
       <h1 className="text-4xl font-semibold">Resume maker</h1>
-      <Button>Get started</Button>
+      <Button onClick={() => router.push("/edit")}>Get started</Button>
     </main>
   );
 }
